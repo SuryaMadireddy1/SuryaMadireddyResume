f71d3fc68dfe713fb39b8e706ea7060b57d56c85
suryamadireddy1 <msvpavanreddy@gmail.com>
2025-02-16 16:52:50 -0500

    Fixed VisitorCounter

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://u6g7y30ag0.execute-api.us-east-1.amazonaws.com/prod/visitorcount")
+    fetch("https://u6g7y30ag0.execute-api.us-east-1.amazonaws.com/prod/visitor")
         .then(response => response.json())
         .then(data => {
             document.getElementById("visitor-counter").textContent = `Visitors: ${data.visits}`;
